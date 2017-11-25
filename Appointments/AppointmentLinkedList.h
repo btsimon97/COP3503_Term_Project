6422f05e435ffa1ae0bb7ca5872e019cb9dfa48f
Jshel <32618925+Jshel@users.noreply.github.com>
2017-11-24 19:06:59 -0500

    Update AppointmentLinkedList.h

--- Appointments/AppointmentLinkedList.h
+++ Appointments/AppointmentLinkedList.h
@@ -3,7 +3,11 @@ public:
 	void printAppointments();
 	Appointment* getHeadAppointment();
 	Appointment* getTailAppointment();
-	void addAppointment(string name, int id);
+	//Add and cancel functions added to the linked list different from UML diagram because they seemed more 
+	//appropriate in the AppointmentLinkedList class.
+	//To add an appointment, the class needs a date and worker as well as visitor information. Changed from UML
+	//diagram.
+	void addAppointment(string name, int id, int day, int month, int time, Worker* worker);
 	void cancelAppointment(string name, int id);
 	void cancelAppointment(int day, int month, int time);
 	AppointmentLinkedList();
