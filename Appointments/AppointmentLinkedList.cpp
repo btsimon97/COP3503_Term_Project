635ab409f39b28e0e95d994800dfd8a4ccb460b5
Jshel <32618925+Jshel@users.noreply.github.com>
2017-11-24 20:23:07 -0500

    Update AppointmentLinkedList.cpp

--- Appointments/AppointmentLinkedList.cpp
+++ Appointments/AppointmentLinkedList.cpp
@@ -18,14 +18,44 @@ Appointment* AppointmentLinkedList::getTailAppointment(){
 }
 
 void addAppointment(string name, int id, int day, int month, int time, Worker* worker){
-	Appointment* newAppointment = new Appointment(month, day, time, worker, name, id);
-	this->tail->setNextAppointment(newAppointment);
-	this->tail = newAppointment;
+	this->tail->setNextAppointment(new Appointment(month, day, time, worker, name, id));
+	this->tail = tail->getNextAppointment();
 	if(this->head == NULL){
 		this->head = this->tail;
 	}
 	//Can be used for debugging and confirmation of addition to user.
 	cout << "Successfully added appointment." << endl;
+	return;
+}
+
+void cancelAppointment(string name, int id){
+	Appointment* current = head;
+	if(current->getVisitor()->getVisitorName() == name && current->getVisitor()->getVisitorID() == id){
+		if(current == this->tail){
+			tail = current->getNextAppointment();
+		}
+		this->head = current->getNextAppointment();
+		delete  current;
+		cout << "Successfully cancelled appointment." << endl;
+		return;
+	}
+	else{
+		//Need to check from the next appointment to relink the list.
+		while(current->getNextAppointment() != NULL){
+			if(current->getNextAppointment()->getVisitor()->getVisitorName() == name && current->getNextAppointment()->getVisitor()->getVisitorID() == id){
+				Appointment* next = current->getNextAppointment()->getNextAppointment();
+				delete current->getNextAppointment();
+				current->setNextAppointment(next);
+				if(next == NULL){
+					this->tail = current;
+				}
+				cout << "Successfully cancelled appointment." << endl;
+				return;
+			}
+			current = current->getNextAppointment();
+		}
+	}
+	cout << "Appointment not found." << endl;
 	return;
 }
 
