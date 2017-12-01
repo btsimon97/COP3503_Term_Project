2d39d6fb458dcd14b61f59b461d2c1776c390289
Dmara34 <32003696+Dmara34@users.noreply.github.com>
2017-12-01 15:08:35 -0500

    Add files via upload

--- People_Management/WorkerLinkedList.cpp
+++ People_Management/WorkerLinkedList.cpp
@@ -13,17 +13,49 @@ private:
 	Worker *head;
 	Worker *tail;
 public:
-	void printWorkerWeekSchedule(std::string name,int id, int month, int weekOfMonth){
+	void printWorkerWeekSchedule(std::string name,int id,int year, int month, int weekOfMonth){
 		Worker* cursor = head;
 		while(cursor->workerID!=id){
 			cursor = cursor->getNextWorkerInList();
 		}
 		if(cursor->getNextWorkerInList()==NULL){
-			cout << "Could not find the worker with that id";
+			cout << "Could not find the worker with that id"<<endl;
 		}
 		else{
-			cout<< cursor->workerName<<"\n";
+			int firstDayToScan;
+			int lastDayToScan;
+			int lastMonthToScan=month;
+			int lastYearToScan=year;
+			firstDayToScan=((weekOfMonth-1)*7)+1;
+			int lastDayToScan=firstDayToScan+6;
+			if(lastDayToScan>Appointment::daysInMonth()){
+				lastDayToScan = lastDayToScan- Appointment::daysInMonth();
+				lastMonthToScan++;
+				if(lastMonthToScan==13){
+					lastYearToScan++;
+					lastMonthToScan=1;
+				}
+			}
+			Appointment currentAppointment = Worker::appointments.head;
+			if(((currentAppointment.Appointment::getDay()>=firstDayToScan)&&(currentAppointment.Appointment::getMonth()==month))||(
+					(currentAppointment.Appointment::getDay()<=lastDayToScan)&&
+					(currentAppointment.Appointment::getMonth()==lastMonthToScan)&&
+					(currentAppointment.Appointment::getYear()==lastYearToScan))
+					){
+				currentAppointment.printAppointment();
+			}
+			while(currentAppointment.getNextAppointment()!=NULL){
+				if(((currentAppointment.Appointment::getDay()>=firstDayToScan)&&(currentAppointment.Appointment::getMonth()==month))||(
+									(currentAppointment.Appointment::getDay()<=lastDayToScan)&&
+									(currentAppointment.Appointment::getMonth()==lastMonthToScan)&&
+									(currentAppointment.Appointment::getYear()==lastYearToScan))
+									){
+					currentAppointment.printAppointment();
+				}
+			}
+
 		}
+
 	}
 	Worker* getHeadWorker(){
 		return head;
