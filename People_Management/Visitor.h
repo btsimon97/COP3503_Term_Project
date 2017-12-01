61808650eabf7f471b29e038fc2537012d7cfb36
Dmara34 <32003696+Dmara34@users.noreply.github.com>
2017-11-30 19:58:54 -0500

    Add files via upload

---
+++ People_Management/Visitor.h
@@ -0,0 +1,26 @@
+/*
+ * Visitor.h
+ *
+ *  Created on: Nov 30, 2017
+ *      Author: David
+ */
+
+#ifndef VISiTOR_H_
+#define VISiTOR_H_
+
+namespace std {
+
+class Visitor {
+private:
+	std::string visitorName;
+	int visitorID;
+public:
+	void printVisitorInfo();
+	int getVisitorId();
+	std::string& getVisitorName();
+	Visitor(std::string name, int id);
+};
+
+}
+
+#endif /* VISiTOR_H_ */
