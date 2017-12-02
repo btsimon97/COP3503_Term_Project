0c6b646908090f590705cb267baa8d1b9e823147
Jshel <Jshel@git.com>
2017-12-01 19:41:08 -0500

    Altered ScheduleManager files and Appointment files for DB use.

--- Appointments/Appointment.cpp
+++ Appointments/Appointment.cpp
@@ -1,8 +1,5 @@
 #include "Appointment.h"
 #include <iostream>
-//To get current date and time.
-#include <ctime>
-#include <stdlib.h>
 using namespace std;
 
 Visitor* Appointment::getVisitor(){
@@ -25,56 +22,6 @@ int Appointment::getAppointmentID(){
 	return appointmentID;
 }
 
-int Appointment::daysInMonth(){
-	switch(month){
-		case 1:
-		return 31;
-		break;
-		case 2:
-		if(year%4 == 0 && (year%100 != 0 || year%400 == 0)){
-			return 29;
-		}
-		else{
-			return 28;
-		}
-		break;
-		case 3:
-		return 31;
-		break;
-		case 4:
-		return 30;
-		break;
-		case 5:
-		return 31;
-		break;
-		case 6:
-		return 30;
-		break;
-		case 7:
-		return 31;
-		break;
-		case 8:
-		return 31;
-		break;
-		case 9:
-		return 30;
-		break;
-		case 10:
-		return 31;
-		break;
-		case 11:
-		return 30;
-		break;
-		case 12:
-		return 31;
-		break;
-		default:
-		cout << "Invalid month." << endl;
-		return -1;
-		break;
-	}
-}
-
 int Appointment::getYear(){
 	return year;
 }
@@ -83,43 +30,26 @@ bool Appointment::getOffDay(){
 	return offDay;
 }
 
-int Appointment::getTimeSlot(){
-	return timeSlot;
+int Appointment::getAppointmentTime(){
+	return appointmentTime;
 }
 
 void Appointment::printAppointment(){
+	cout << "Appointment Time: " << appointmenTime << " hours" << endl;
+	cout << endl;
 	if(!offDay){
-		cout << "Appointment Information: " << endl;
+		cout << "Visitor Name: " << visitor->getVisitorName() << endl;
+		cout << "Visitor ID Number: " << visitor->getVisitorID() << endl;
 		cout << endl;
-		cout << "Appointment holding visitor: " << endl;
-		cout << "Name: " << visitor->getVisitorName() << endl;
-		cout << "ID Number: " << visitor->getVisitorID() << endl;
-		cout << endl;
-		cout << "Appointment holding worker: " << endl;
-	//Need functions to access the worker's information.
-		cout << "Name: " << worker->getWorkerName() << endl;
-		cout << "ID Number: " << worker->getWorkerID() << endl;
 	}
 	else{
-		cout << worker->getWorkerName() << " has a scheduled day off on  " << this->getDay() << "\\" << this->getMonth() << "\\" << this->getYear() << endl;
+		cout << "Scheduled time off " << endl;
+		cout << endl;
 	}
 	return;
 }
 
 Appointment::Appointment(bool offDay, int day, int month, int year, int appointmentTime, Worker* worker, string visitorName, int visitorID){
-	//To check time inputs.
-	time_t t = time(0);
-	struct tm* localTime = localtime(&t);
-	//tm_year starts counting from 1900 and tm_mon starts months at 0.
-	if(year < (localTime->tm_year + 1900) || ((year == localTime->tm_year + 1900) && month < (localTime->tm_mon + 1)) || ((month == localTime->tm_mon + 1) && day < localTime->tm_mday)){
-		throw string("Cannot make appointments in the past.");
-	}
-	else if(month < 1 || month > 12){
-		throw string("Please input a valid month.");
-	}
-	else if(day < 1 || day > this->daysInMonth()){
-		throw string("Please input a valid day.");
-	}
 	//Seed for random number generator with time.
 	srand(time(NULL));
 	this->offDay = offDay;
--- Info_Management/ScheduleManager.cpp
+++ Info_Management/ScheduleManager.cpp
@@ -1,18 +1,25 @@
 #include "ScheduleManager.h"
-#include "Appointment.h"
-#include "Worker.h"
-#include "Visitor.h"
+//For printing the week.
+#include <ctime>
 using namespace std;
 
-ScheduleManager::ScheduleManager(int appointmentLength){
-	//Avoid divide-by-zero error by checking = 0.
-	if(appointmentLength <= 0 || appointmentLength > (dayLength*60)){
-		throw string("Please enter a valid appointment length.");
-	}
-	this->appointmentLength = appointmentLength;
-}
-
 void ScheduleManager::addAppointment(bool offDay, int day, int month, int year, int appointmentTime, Worker* worker, string name, int id, const char* DBFilePath){
+	//To check time inputs.
+	time_t t = time(0);
+	struct tm* localTime = localtime(&t);
+	//tm_year starts counting from 1900 and tm_mon starts months at 0.
+	if(year < (localTime->tm_year + 1900) || ((year == localTime->tm_year + 1900) && month < (localTime->tm_mon + 1)) || ((month == localTime->tm_mon + 1) && day < localTime->tm_mday)){
+		cout << "Cannot make appointments in the past." << endl;
+		return;
+	}
+	else if(month < 1 || month > 12){
+		cout << "Please input a valid month." << endl;
+		return;
+	}
+	else if(day < 1 || day > this->daysInMonth()){
+		cout << "Please input a valid day." << endl;
+		return;
+	}
 	string appointmentDate = to_string(month) + "//";
 	if(numberOfDigits(day) == 1){
 		appointmentDate += ("0" + to_string(day));
@@ -27,6 +34,7 @@ void ScheduleManager::addAppointment(bool offDay, int day, int month, int year, 
 	else{
 		cout << "An appointment has already been scheduled for this time." << endl;
 	}
+	return;
 }
 
 void ScheduleManager::cancelAppointment(int id, const char* DBFilePath){
@@ -64,8 +72,75 @@ void ScheduleManager::cancelAppointment(int day, int month, int year, int appoin
 	}
 }
 
-void ScheduleManager::printWorkerWeekSchedule(){
-
+//Prints the worker's schedule for the current week.
+void ScheduleManager::printWorkerWeekSchedule(Worker* worker, const char* DBFilePath){
+	//Find the current week.
+	time_t t = time(0);
+	struct tm* localTime = localtime(&t);
+	//daysPastSunday is between 0 and 6 corresponding to Sunday to Saturday.
+	int daysPastSunday = localTime->tm_wday;
+	int startDay;
+	int startMonth;
+	int startYear;
+	if(localTime->tm_mday - daysPastSunday <= 0){
+		if(localTime->tm_mon == 0){
+			startMonth = 12;
+			startDay = daysInMonth(12) - daysPastSunday;
+			startYear = localTime->tm_year +1900 - 1;
+		}
+		else{
+			//Don't need to subtract a month because the months start from 0 in tm and will use months starting from 1.
+			startMonth = localTime->tm_mon;
+			startDay = daysInMonth(startMonth) - daysPastSunday;
+			startYear = localTime->tm_year + 1900;
+		}
+	}
+	else{
+		startMonth = localTime->tm_mon + 1;
+		startDay = localTime->tm_mday - daysPastSunday;
+		startYear = localTime->tm_year + 1900;
+	}
+	cout << worker->getWorkerName() << "'s Schedule for the week of " << startDay<< "/" << startMonth << "/" << localTime->tm_year << ":" << endl;
+	for(int i = 0; i < 7; i++){
+		switch(daysPastSunday){
+			case 0:
+			cout << "Sunday Appointments:" << endl;
+			break;
+			case 1:
+			cout << "Monday Appointments:" << endl;
+			break;
+			case 2:
+			cout << "Tuesday Appointments:" << endl;
+			break;
+			case 3:
+			cout << "Wednesday Appointments:" << endl;
+			break;
+			case 4:
+			cout << "Thursday Appointments:" << endl;
+			break;
+			case 5:
+			cout << "Friday Appointments:" << endl;
+			break;
+			case 6:
+			cout << "Saturday Appointments:" << endl;
+			break;
+		}
+		printWorkerDaySchedule(worker, startDay, startMonth, startYear, DBFilePath);
+		if(startDay + 1 > daysInMonth(startMonth)){
+			if(startMonth == 12){
+				startYear++;
+				startMonth = 1;
+			}
+			else{
+				startMonth++;
+			}
+			startDay = 1;
+		}
+		else{
+			startDay++;
+		}
+	}
+	return;
 }
 
 void ScheduleManager::printWorkerDaySchedule(Worker* worker, int day, int month, int year, const char* DBFilePath){
@@ -82,8 +157,11 @@ void ScheduleManager::printWorkerDaySchedule(Worker* worker, int day, int month,
 		cout << "No appointments on " << appointmentDate << endl;
 	}
 	else{
+		cout << worker->getWorkerName() << " has the following appointments on " << appointmentDate << ": " << endl;
 		Appointment** dayAppointments = new Appointment*[meetingNumber];
+		//findAppointments is sorted by appointment ID, not chronologically.
 		findAppointments(DBFilePath, worker->getWorkerID(), appointmentDate.c_str(), dayAppoinments);
+		chronologicalSort(dayAppointments);
 		for(int i = 0; i < meetingNumber; i++){
 			dayAppointments[i]->printAppointment();
 		}
@@ -91,10 +169,22 @@ void ScheduleManager::printWorkerDaySchedule(Worker* worker, int day, int month,
 	return;
 }
 
-void ScheduleManager::scheduleNearestAppointment(){
-
+void ScheduleManager::chronologicalSort(Appointment** unsorted, int unsortedSize){
+	//The array is already sorted if it has one element. Avoid out of bounds errors.
+	if(unsortedSize > 1){
+		for(int i = unsortedSize - 1; i >= 0, i--){
+			for(int j = 0; j < unsortedSize - i - 1; j++){
+				if(unsorted[j]->getAppointmentTime() > unsorted[i]->getAppointmentTime()){
+					Appointment* temp = unsorted[j];
+					unsorted[j] = unsorted[i];
+					unsorted[i] = temp;
+				}
+			}
+		}
+	}
+	return;
 }
-
+//Ensure days of month have two digits for proper formatting.
 void ScheduleManager::numberOfDigits(int i){
 	//The inputted value will have at least one digit.
 	int digitNumber = 1;
@@ -103,4 +193,54 @@ void ScheduleManager::numberOfDigits(int i){
 		i /= 10;
 	}
 	return digitNumber;
+}
+
+int ScheduleManager::daysInMonth(int month){
+	switch(month){
+		case 1:
+		return 31;
+		break;
+		case 2:
+		if(year%4 == 0 && (year%100 != 0 || year%400 == 0)){
+			return 29;
+		}
+		else{
+			return 28;
+		}
+		break;
+		case 3:
+		return 31;
+		break;
+		case 4:
+		return 30;
+		break;
+		case 5:
+		return 31;
+		break;
+		case 6:
+		return 30;
+		break;
+		case 7:
+		return 31;
+		break;
+		case 8:
+		return 31;
+		break;
+		case 9:
+		return 30;
+		break;
+		case 10:
+		return 31;
+		break;
+		case 11:
+		return 30;
+		break;
+		case 12:
+		return 31;
+		break;
+		default:
+		cout << "Invalid month." << endl;
+		return -1;
+		break;
+	}
 }
