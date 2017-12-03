28dc57d1cdfb294c4c672c8e6866bb68ba4388a8
Jshel <32618925+Jshel@users.noreply.github.com>
2017-12-02 19:24:12 -0500

    Update Visitor.h

--- People_Management/Visitor.h
+++ People_Management/Visitor.h
@@ -5,24 +5,18 @@
  *      Author: David
  */
 
-
-#ifndef VISiTOR_H_
-#define VISiTOR_H_
 #include <string>
-
-namespace std {
-
+using namespace std;
+#ifndef VISITOR_H
+#define VISITOR_H
 class Visitor {
 private:
-	std::string visitorName;
+	string visitorName;
 	int visitorID;
 public:
-	void printVisitorInfo();
-	int getVisitorId();
-	std::string& getVisitorName();
-	Visitor(std::string name, int id);
+	int getVisitorID();
+	string getVisitorName();
+	Visitor(string name, int id);
 };
 
-}
-
-#endif /* VISiTOR_H_ */
+#endif /* VISITOR_H */
